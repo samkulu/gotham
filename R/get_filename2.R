d2765a0fa3bbac87025191fd1d6decdb88cc628d
Meichtry Samuel BJ <samuel.meichtry@bj.admin.ch>
2025-01-27 10:18:33 +0100

    NEW get_filename2 - without id in filenaming

---
+++ R/get_filename2.R
@@ -0,0 +1,30 @@
+#' Get Filename
+#'
+#' @param x URL for page response
+#' @param id Post id
+#' @param dest Destination where to store file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' get_filename2(url)
+get_filename2 <- function(x, dest){
+  m <- gregexpr("20[0-9]{2}\\/[0-9]{2}\\/[0-9]{2}",x)
+  dt <- as.Date(gsub("\\/","-",regmatches(x, m)))
+  filename <- paste(format(dt, "%Y%m%d"),"_", basename(x),".html",sep="")
+  if(class(dt) == "Date"){
+    if(length(grep("gothamcity\\.fr", x)) == 1)
+    fullname <- file.path(dest, format(dt, "%Y"), "AboFR",filename)
+    else
+      fullname <- file.path(dest, format(dt, "%Y"), filename)
+  } else {
+    # Missing Date info
+    browser()
+    stop("Missing Date info!")
+  }
+
+  # Return
+  fullname
+}
